1c0c7fbae56fb0b70061de5ffed6a6183ae75de7
enGVee <35076961+goldexer@users.noreply.github.com>
2025-07-31 13:26:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,104 +1,15 @@
 'use strict';
 
-const dbc = require("./controller_Database")
 const http = require("http")
 const url = require('url');
-const crypto = require('crypto');
-//crypto.createHash('sha256').update('bacon').digest('hex');
-
-dbc.initUserTable()
 
 const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-
-    function BadRequest() {
-        console.log("Bad request registered!")
-        res.statusCode = 400; res.end();
-    }
-
-    function BadCredentials() {
-        return (parsedUrl.query?.username?.length < 2 || parsedUrl.query?.userpass?.length < 6);
-    }
-
-    //---------------------------- Request info ----------------------------------------------
-    console.log("Incoming message... method=" + req.method + " path=" + parsedUrl.pathname)
 
     if (req.method === 'GET'){
-        console.log(parsedUrl.query)
-
-
-        switch (parsedUrl.pathname) {
-            //----------------------- Registration -----------------------------------------------
-            case '/processReg': {
-
-                // Server credentials validation
-                if (BadCredentials()) { BadRequest(); break; }
-                //Good credentials, continue registration
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-
-                console.log("good credentials, pass")
-
-                const Hash = crypto.createHash('sha256').update(parsedUrl.query.username + parsedUrl.query.userpass);
-
-                dbc.addUser(parsedUrl.query.username, Hash.digest('hex'), result => {
-                    console.log("add user go result = " + result)
-                    switch (result) {
-                        case 'exists': {
-                            res.end(JSON.stringify( {'username': '', 'userpass': '', 'result': 'exists'} ));
-                            break;
-                        }
-                        case 'success': {
-                            res.end(JSON.stringify( {'username': '', 'userpass': '', 'result': 'success'} ));
-                            break;
-                        }
-                    }
-                })
-
-                res.end(JSON.stringify( { 'username' : parsedUrl.query.username,
-                                          'userpass' : parsedUrl.query.userpass,
-                                          'error'    : 'none'}
-                ));
-                break;
-            }
-
-            //---------------------- Authorization -----------------------------------------------
-            case '/processLog': {
-                console.log('Logon request')
-
-                if (BadCredentials()) { BadRequest(); break; }
-
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-
-                const Hash = crypto.createHash('sha256').update(parsedUrl.query.username + parsedUrl.query.userpass);
-
-                const requestToken = Hash.digest('hex').toString();
-
-                console.log(requestToken)
-                dbc.getUserToken(parsedUrl.query.username, (databaseToken) => {
-                    console.log(databaseToken)
-
-                    if (requestToken === databaseToken) {
-                        console.log("Есть пользователь!")
-                        res.statusCode = 200;
-                        res.end(JSON.stringify({
-                                'token': databaseToken,
-                                'error': 'none'
-                            }
-                        ));
-                    } else {
-                        res.statusCode = 200;
-                        res.end(JSON.stringify({'error': 'denied'}));
-                    }
-                })
-                break;
-            }
-
-            //-------------------- Bad or unusual request ----------------------------------------
-            default: BadRequest()
-        }
-    }
+        
+	res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/text');
+	res.end("Hello World!");
 })
 
 server.setTimeout(10000);
